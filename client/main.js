a743938f1779dd578c19d438dd512847778dc2c6
peidong yuan <yuanpeidong1991@gmail.com>
2016-04-06 00:14:01 +0800

    renew project

--- client/main.js
+++
@@ -1,7 +0,0 @@
-import { Template } from 'meteor/templating';
-
-import '../imports/api/tasks.js';
-
-import '../imports/ui/body.js';
- 
-import '../imports/startup/accounts-config.js';
--- imports/api/tasks.js
+++
@@ -1,6 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
- 
-export const Tasks = new Mongo.Collection('tasks');
-
